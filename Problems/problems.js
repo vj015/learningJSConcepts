bc21e8cf5a93ec29f74bbd626ae81baa9f1863de
Vipul Jain <vjain2376@gmail.com>
2024-05-26 20:39:37 +0530

    Execute in series

--- Problems/problems.js
+++ Problems/problems.js
@@ -1,1 +1,11 @@
 //Execute async functions in Series
+const executeInSeries = async (promises) => {
+  for (const p of promises) {
+    try {
+      let res = await p;
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
