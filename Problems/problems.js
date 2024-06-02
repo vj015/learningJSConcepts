98e96ae6e64660955653170447018cbb0cd9cc1b
Vipul Jain <vjain2376@gmail.com>
2024-06-02 14:29:17 +0530

    updated code

--- Problems/problems.js
+++ Problems/problems.js
@@ -1,13 +1,40 @@
-//Execute async functions in Series
-const executeInSeries = async (promises) => {
+//Execute async functions in Series  Method 1
+const executeInSeries1 = async (promises, cb) => {
+  const success = [];
+  const errors = [];
   for (const p of promises) {
     try {
       let res = await p;
-      console.log(res);
+      success.push(res);
     } catch (error) {
-      console.log(error);
+      errors.push(error);
     }
   }
+  cb(errors, success);
+};
+
+//Execute async functions in Series  Method 2
+const executeInSeries2 = (promises, cb) => {
+  const success = [];
+  const errors = [];
+  let completed = 0;
+  promises.reduce((prev, curr) => {
+    prev.finally(() => {
+      curr
+        .then((val) => {
+          success.push(val);
+        })
+        .catch((err) => {
+          errors.push(err);
+        })
+        .finally(() => {
+          completed++;
+          if (completed >= promises.length) {
+            cb(errors, success);
+          }
+        });
+    });
+  }, Promise.resolve());
 };
 
 const executeInParallel = (promises, cb) => {
