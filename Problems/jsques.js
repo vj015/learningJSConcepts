d2886ad77245fded1e0663423ddbda7fbd6768c8
Vipul Jain <vjain2376@gmail.com>
2024-08-17 17:57:10 +0530

    added

--- Problems/jsques.js
+++ Problems/jsques.js
@@ -57,3 +57,32 @@ function throttle(fn, delay) {
     }
   };
 }
+
+//calculator.add(10).subtract(2).divide(2).multiply(5);
+function calculator() {
+  this.value = 0;
+  this.add = (val) => {
+    this.value += val;
+    return this;
+  };
+  this.subtract = (val) => {
+    this.value -= val;
+    return this;
+  };
+  this.multiply = (val) => {
+    this.value *= val;
+    return this;
+  };
+  this.divide = (val) => {
+    if (val === 0) {
+      return this;
+    }
+    this.value /= val;
+    return this;
+  };
+  this.print = () => {
+    return this.value;
+  };
+}
+let a = new calculator().add(10).subtract(2).divide(2).multiply(5).print();
+console.log(a);
